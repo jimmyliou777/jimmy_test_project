8f75f9220989519f073123defd1cbb64b4906a07
jimmyliou <jimmyliou@boyu66.net>
2021-03-21 20:18:24 +0800

    feature: add prettier rules and settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Home(): JSX.Element {
-  return (
-    <ul>
-      <li>
-        <Link href="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
-  )
+    return (
+        <ul>
+            <li>
+                <Link href="/a">
+                    <a>a</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/b">
+                    <a>b</a>
+                </Link>
+            </li>
+        </ul>
+    );
 }
