e916e20cef182ab52003cdb7c5030c68fca9b6c5
jimmyliou <jimmyliou@boyu66.net>
2021-04-29 17:07:15 +0800

    feature: init sass module

--- src/components/example/index.tsx
+++ src/components/example/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from '~/styles/example.scss';
 
 export default function Example(): JSX.Element {
-    return <div className={'global-container ' + styles['local-container']}> Test Scss </div>;
+    return <div> Test Scss </div>;
 }
